048487cd83ef6b783fb473b6f4fb5f94120a4cab
Daniel Arias <128391731+das3110@users.noreply.github.com>
2024-09-01 23:26:58 -0400

    baseV1

---
+++ src/CMakeFiles/Reversi.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Reversi.dir/board.cpp.o"
+  "CMakeFiles/Reversi.dir/board.cpp.o.d"
+  "CMakeFiles/Reversi.dir/main.cpp.o"
+  "CMakeFiles/Reversi.dir/main.cpp.o.d"
+  "Reversi"
+  "Reversi.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Reversi.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
